df3182282b8ca9b21754668b144e9d62156b7f0e
Mateusz Szczech <matle0410@gmail.com>
2024-01-15 16:24:07 +0100

    Configuration changes

--- LuaSharp/Program.cs
+++ LuaSharp/Program.cs
@@ -1,23 +1,34 @@
-﻿
-string? line;
-
-try
+﻿class Program
 {
-    StreamReader sr = new StreamReader("D:\\Interpreter\\LuaSharp\\LuaSharp\\hello.lua");
-    line = sr.ReadLine();
-    while (line != null)
+    public static void Main(string[] args)
     {
-        Console.WriteLine(line);
-        line = sr.ReadLine();
+        foreach (var arg in args)
+        {
+            Console.WriteLine(arg);
+        }
+
+        string path = "D:\\Interpreter\\LuaSharp\\LuaSharp\\hello.lua";
+
+        try
+        {
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Error no file under path" + path);
+            }
+
+            using (StreamReader sr = new StreamReader(path))
+            {
+                while (sr.Peek() >= 0)
+                {
+                    Console.Write((char)sr.Read());
+                }
+            }
+
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
     }
-    sr.Close();
-    Console.ReadLine();
-}
-catch (Exception e)
-{
-    Console.WriteLine("Exception: " + e.Message);
-}
-finally
-{
-    Console.WriteLine("Executing finally block.");
 }
