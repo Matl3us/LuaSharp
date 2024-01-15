7a3fbde503a6a704209725ab56110bac7d2ab2d1
Mateusz Szczech <matle0410@gmail.com>
2024-01-15 16:08:59 +0100

    Initial configuration

---
+++ LuaSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿
+string? line;
+
+try
+{
+    StreamReader sr = new StreamReader("D:\\Interpreter\\LuaSharp\\LuaSharp\\hello.lua");
+    line = sr.ReadLine();
+    while (line != null)
+    {
+        Console.WriteLine(line);
+        line = sr.ReadLine();
+    }
+    sr.Close();
+    Console.ReadLine();
+}
+catch (Exception e)
+{
+    Console.WriteLine("Exception: " + e.Message);
+}
+finally
+{
+    Console.WriteLine("Executing finally block.");
+}
